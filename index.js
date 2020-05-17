d8a8e8a5848a5f01e6cf9fe3d621ec2cb02feaf6
Ashraf Bawer <ashraf.bawer1@gmail.com>
2020-05-17 10:13:46 +0430

    PORT environemnt variable

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 
 
-
 app.get('/', (req,res) => {
     res.send({ start: "Hello There!" });
 });
 
 
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
